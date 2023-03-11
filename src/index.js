ee5f54af166b2587796f60d6bbd0adb33d3272d1
calebchris000 <calebchris000@gmail.com>
2023-03-11 14:00:17 +0100

    Implement API to fetch data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./style.css"
+require('./modules/renderMovies.js')
---
+++ src/modules/renderMovies.js
@@ -0,0 +1,43 @@
+const baseAPI = "https://api.tvmaze.com/shows";
+
+const getJSON = async () => {
+  const request = await fetch(baseAPI);
+  const response = await request.json();
+
+  return response
+};
+
+
+
+getJSON()
+
+const render = async () => {
+  const container = document.querySelector(".container");
+  const jsonData = await getJSON();
+    let item = ''
+  for (let i = 0; i < 6; i++) {
+    item += `
+     <div class="item">
+            <img src=${jsonData[i].image.medium} alt="">
+
+            <div class="description">
+                <p class="name">${jsonData[i].name}</p>
+
+                <div class="like">
+                    <i class="fa-regular fa-heart"></i>
+                    <p>5 likes</p>
+                </div>
+
+
+            </div>
+
+            <button>Comment</button>
+            <button>Reservation</button>
+        </div>`
+  }
+
+  container.innerHTML = item;
+};
+
+render()
+
